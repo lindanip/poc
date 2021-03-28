229314fd6df511915c2406533c4ddbe0447bf702
lindanip <lindanip540@gmail.com>
2021-03-28 23:00:34 +0200

    a

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '01058a3f544b5186b201f62da25639fe'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0198efdc-0d26-413a-b008-c12ca6dcb7c8'
+};
